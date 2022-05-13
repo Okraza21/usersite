88f2f38164002d466871462fae1c97b6b33649c9
Sergey Azarko <sokraza73@gmail.com>
2022-05-14 02:26:53 +0300

    add news/create

--- usersite/news/views.py
+++ usersite/news/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Articles
+from .forms import ArticlesForm
 
 
 def news_home(request):
-    return render(request, 'news/news_home.html')
+    news = Articles.objects.all()
+    return render(request, 'news/news_home.html', {'news': news})
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+
+
+    form = ArticlesForm()
+    data = {
+            'form': form
+    }
+
+    return render(request, 'news/create.html', data)
